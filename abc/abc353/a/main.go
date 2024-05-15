8a264b0d87972fe04710df4a5eb0ba84213ba135
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-05-15 11:20:15 +0900

    Add A from ABC353

---
+++ abc/abc353/a/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	maxN   = 2 * 100000
+	maxVal = 1000000000
+)
+
+var (
+	sc     = bufio.NewScanner(os.Stdin)
+	buffer = make([]byte, maxN*12)
+)
+
+func scanStr() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func scanInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
+func scanLineStr() []string {
+	s := scanStr()
+
+	return strings.Split(s, " ")
+}
+
+func scanLineInt() []int {
+	s := scanStr()
+	strList := strings.Split(s, " ")
+
+	slice := make([]int, len(strList))
+	for i, v := range strList {
+		num, _ := strconv.Atoi(string(v))
+		slice[i] = num
+	}
+
+	return slice
+}
+
+func main() {
+	sc.Buffer(buffer, maxN*12)
+
+	n := scanInt()
+	h := scanLineInt()
+
+	first := h[0]
+	for i := 0; i < n; i++ {
+		if h[i] > first {
+			fmt.Println(i + 1)
+			return
+		}
+	}
+	fmt.Println(-1)
+}
